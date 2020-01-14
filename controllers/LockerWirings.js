9f4c0f79da5435e14020f679513eaf403900747f
zfima <zzfima@gmail.com>
2020-01-14 10:27:49 +0200

    fix bug

--- controllers/LockerWirings.js
+++ controllers/LockerWirings.js
@@ -3,5 +3,7 @@ module.exports.GetVersion = function(req, res){
     let container = require('../containerConfig')
     let version = container.get('versionProvider')
     let answer = version.GetVersion()
-    res.end(answer) 
+
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(answer)) 
 }
