c948b4193ea3f5e39f628a0a70b4b627b12a4e60
zfima <zzfima@gmail.com>
2020-01-15 13:19:12 +0200

    rfct

--- containerConfig.js
+++ containerConfig.js
@@ -1,5 +1,5 @@
 let container = require('kontainer-di')
 module.exports = container
-let versionProvider = require('./services/versionProvider')
+let versionProvider = require('./services/VersionProvider')
 
 container.register('versionProvider', [], versionProvider)
