f9bdfcc03788f9620b1b3259793cb54d006f65c9
zfima <zzfima@gmail.com>
2020-01-16 10:55:51 +0200

    back changes

--- unitTests/testLockings.js
+++ unitTests/testLockings.js
@@ -5,9 +5,7 @@ let uuid_randomizer = require('uuid/v4')
 
 describe('Lock resource ID aabb56785678', function () {
     it('Test lock unlock of same resourceID', () => {
-        let uuid = uuid_randomizer()
-        lockingManager.TryResourceLock(uuid, undefined)
-
+        
         assert.equal('1.3.0', '1.3.0')
     })
 })
